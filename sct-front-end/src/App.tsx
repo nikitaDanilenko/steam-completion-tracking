02bcc7095b6b8ba61da5e6078b3e4677865d0f13
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2025-08-18 22:33:53 +0200

    Set up minimal Vite project

---
+++ sct-front-end/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <header>
+        <h1>Steam Completion Tracker</h1>
+      </header>
+    </main>
+  )
+}
+
+export default App
